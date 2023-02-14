ae0d23dc540f125dba442bd717d34c55c0b4b6b2
themisterpig <josedfl2001@gmail.com>
2023-02-14 20:32:52 +0000

    Update PLusOne.py

--- Easy/.ipynb_checkpoints/PLusOne.py
+++ Easy/.ipynb_checkpoints/PLusOne.py
@@ -1,5 +1,13 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        for a in digits:
+digits = [1,2,3]
 
+def plusOne(digits):
+    num = 0
+    for i in range(len(digits)):
+        num = num * 10 + digits[i]
+    num += 1
+    return [int(i) for i in str(num)]
 
+print(plusOne(digits))
+
+def addbinary(a,b):
+    return bin(int(a,2)+int(b,2))[2:]
